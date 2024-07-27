742950766690f042e91582e221a9652d36614dfb
shilaca <shilaca.fu@gmail.com>
2024-07-27 14:04:12 +0900

    add todo page ver

--- tests/constants.ts
+++ tests/constants.ts
@@ -1,1 +1,23 @@
 export const SITE_URL = "https://todo-app-qajp.vercel.app/";
+
+export const USER = {
+  "ue-test01": {
+    email: "ue-test01@e.com",
+    password: "test123",
+  },
+
+  "ue-test02": {
+    email: "ue-test02@e.com",
+    password: "test123",
+  },
+
+  "ue-test09": {
+    email: "ue-test09@e.com",
+    password: "test123",
+  },
+
+  "ue-test10": {
+    email: "ue-test10@e.com",
+    password: "test123",
+  },
+};
